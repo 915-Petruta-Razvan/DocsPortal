91ebbbf44ccc4f728706bbefe25be587762aca23
razvanpetruta <razvan.petruta@stud.ubbcluj.ro>
2025-06-14 21:38:44 +0300

    added redis package and named containers

--- DocsPortal.AppHost/Program.cs
+++ DocsPortal.AppHost/Program.cs
@@ -2,13 +2,20 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(ar
 
 IResourceBuilder<SqlServerServerResource> sql = builder
     .AddSqlServer("sql")
-    .WithLifetime(ContainerLifetime.Persistent);
-
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithContainerName("docsportal-sql");
 IResourceBuilder<SqlServerDatabaseResource> db = sql.AddDatabase("database");
+
+IResourceBuilder<RedisResource> redis = builder
+    .AddRedis("redis")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithContainerName("docsportal-redis");
 
 builder
     .AddProject<Projects.DocsPortal_Api>("docsportal-api")
     .WithReference(db)
-    .WaitFor(db);
+    .WithReference(redis)
+    .WaitFor(db)
+    .WaitFor(redis);
 
 builder.Build().Run();
